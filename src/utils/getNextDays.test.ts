03b1ec24ccae635b4179ee7765d86ded30c5cdcf
van-gomes <nninha.gomes@gmail.com>
2024-07-06 18:18:12 -0300

    Starting study tests

--- src/utils/getNextDays.test.ts
+++ src/utils/getNextDays.test.ts
@@ -3,6 +3,7 @@ import { getNextDays } from './getNextDays';
 describe('getNextDays', () => {
   it("should be return the next five days", () => {
     const days = getNextDays();
+    //console.log(days);
   
     expect(days.length).toBe(5)
   })
