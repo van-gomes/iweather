9aa89ed68f8532a337dd48fe946b4e7dbf714933
van-gomes <nninha.gomes@gmail.com>
2024-07-12 16:02:39 -0300

    Test API return

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -4,8 +4,6 @@ import { getCityByNameService } from './getCityByNameService'
 
 describe("Service: getCityByNameService", () => {
   it('should return city details', async () => {
-    
-
     jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse })
 
     const response = await getCityByNameService('São Paulo')
