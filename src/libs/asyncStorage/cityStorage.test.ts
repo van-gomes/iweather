2fb122309cc579e1028b949b6d72be5107dc371a
van-gomes <nninha.gomes@gmail.com>
2024-07-12 16:34:01 -0300

    Storage test

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -10,25 +10,26 @@ const newCity: CityProps = {
 
 describe('Storage: CityStorage', () => {
   it("should be return null when don't have a city storaged", async () => {
-    const response = await getStorageCity()
+    const response = await getStorageCity();
 
-    expect(response).toBeNull()
+    expect(response).toBeNull();
   })
 
   it('shoul be return city storaged', async () => {
     
-    await saveStorageCity(newCity)
+    await saveStorageCity(newCity);
 
-    const response = await getStorageCity()
+    const response = await getStorageCity();
 
-    expect(response).toEqual(newCity)
+    expect(response).toEqual(newCity);
   })
 
   it('should be remove city storage', async () => {    
-    await saveStorageCity(newCity)
-    await removeStorageCity()
+    await saveStorageCity(newCity);
 
-    const response = await getStorageCity()
+    await removeStorageCity();
+
+    const response = await getStorageCity();
 
     expect(response).toBeNull()
   })
