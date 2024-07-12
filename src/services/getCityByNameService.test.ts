985527811943bf330e5432f66e8631f15c830fda
van-gomes <nninha.gomes@gmail.com>
2024-07-12 16:15:05 -0300

    Build mock API using console or debug

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -3,6 +3,10 @@ import { api } from './api'
 import { getCityByNameService } from './getCityByNameService'
 
 describe("Service: getCityByNameService", () => {
+  /* Dica para montar o mock da API:
+  - Usar console log
+  - Usar o debug */
+  
   it('should return city details', async () => {
     jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse })
 
